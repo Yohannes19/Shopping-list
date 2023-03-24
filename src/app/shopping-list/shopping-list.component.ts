bb7a3f055e47daa4d43f75a216b9a7cbbe8be2eb
Yohannes19 <joeabrha@gmail.com>
2023-03-24 13:52:08 +0000

    Adding shopping list section

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingriedents.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
+  ingriedents:Ingredient[]=[
+    new Ingredient('Apples',5),
+    new Ingredient('Tomatos',10)
+  ];
 
 }
